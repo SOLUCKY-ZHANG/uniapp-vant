76a1a4a5683550e93c896df7a47e3180dbd09a67
张东杰 <392218997@qq.com>
2022-06-09 12:56:33 +0800

    初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="content">
+		<van-button @click="openToast">vant 按钮</van-button>
+		<van-toast id="van-toast" />
+	</view>
+</template>
+
+<script>
+	import Toast from '@/wxcomponents/vant/toast/toast'
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+            openToast(){
+				Toast('提示文案')
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
